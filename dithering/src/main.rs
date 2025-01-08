13b26b116736a43916617a22227db42196e3a8d8
ValRom <valentin.romanet@etu.univ-orleans.fr>
2025-01-08 15:21:03 +0100

    feat : suppression du canal alpha de l'image

--- dithering/src/main.rs
+++ dithering/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use image::{io::Reader as ImageReader, DynamicImage, ImageError, ImageOutputFormat};
+use std::io::Cursor;
+
+fn main() -> Result<(), ImageError> {
+    let img = ImageReader::open("image/BUTInfo.jpg")?.decode()?;
+
+    let mut bytes: Vec<u8> = Vec::new();
+    img.write_to(&mut Cursor::new(&mut bytes), ImageOutputFormat::Png)?;
+
+    let rgb = img.to_rgb8();
+    let no_alpha = DynamicImage::ImageRgb8(rgb);
+
+    no_alpha.save("image/no_alpha.jpg")?;
+
+    Ok(())
 }
