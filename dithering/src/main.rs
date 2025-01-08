439b8e6f32c21cbd5cf6a4e4390cde47d92223e3
ValRom <valentin.romanet@etu.univ-orleans.fr>
2025-01-08 15:30:38 +0100

    feat : image un pixel sur deux en blanc

--- dithering/src/main.rs
+++ dithering/src/main.rs
@@ -1,4 +1,4 @@
-use image::{io::Reader as ImageReader, ImageError, ImageOutputFormat};
+use image::{buffer::EnumeratePixelsMut, io::Reader as ImageReader, ImageError, ImageOutputFormat};
 use std::io::Cursor;
 
 fn main() -> Result<(), ImageError> {
@@ -13,6 +13,15 @@ fn main() -> Result<(), ImageError> {
 
     let pixel = rgb.get_pixel(32, 52);
     println!("Pixel (32, 52) : {:?}", pixel);
+
+    let mut white = rgb.clone();
+    for (x, y, pixel) in white.enumerate_pixels_mut() {
+        if x % 2 == 0 && y % 2 == 0 {
+            *pixel = image::Rgb([255, 255, 255]);
+        }
+    }
+
+    white.save("image/white.jpg")?;
 
     Ok(())
 }
